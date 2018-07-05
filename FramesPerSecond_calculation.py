efb621342ef1260ba62d01d97c11a3950726ef6b
Sudhakar Sivanesan <34116450+SudhaSiva@users.noreply.github.com>
2018-07-05 16:17:46 +0800

    Update FramesPerSecond_calculation.py

--- FramesPerSecond_calculation.py
+++ FramesPerSecond_calculation.py
@@ -13,27 +13,11 @@ import time
 if __name__ == '__main__' :
  
     # Start default camera
-    video = cv2.VideoCapture(-1);
-    
-    """  
-    # Find OpenCV version
-    #(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+    video = cv2.VideoCapture(-1);      
      
-    # With webcam get(CV_CAP_PROP_FPS) does not work.
-    # Let's see for ourselves.
-   
-    if int(major_ver)  < 3 :
-        fps = video.get(cv2.cv.CV_CAP_PROP_FPS)
-        print ("Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(fps))
-    else :
-        fps = video.get(cv2.CAP_PROP_FPS)
-        print ("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
-    """
- 
     # Number of frames to capture
     num_frames = 1
-     
-     
+       
     print ("Capturing {0} frames".format(num_frames))
  
     # Start time
